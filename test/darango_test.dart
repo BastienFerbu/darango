e27067219f69be066cf4f46a91179fe6b5f45747
Bastien Ferbu <bastien.ferbu@gmail.com>
2019-10-05 22:09:41 +0200

    remove some useless lines

--- test/darango_test.dart
+++ test/darango_test.dart
@@ -1,6 +1,6 @@
 import 'package:darango/darango.dart';
 
 String url = "http://127.0.0.1:8529";
-String database_name = "zaucard";
-String username = "root";
-String password = "24861793";
+String database_name = "";
+String username = "";
+String password = "";
